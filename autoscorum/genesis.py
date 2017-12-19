6c435a9a2509a01ec711e5da0500c6b9120ac9f0
Ivan Romanovsky <ivan.romanovsky@scorum.com>
2017-12-19 11:38:42 +0300

    block-151 genesis block, update genesis class with registration committee and registration schedule

--- autoscorum/genesis.py
+++ autoscorum/genesis.py
@@ -10,8 +10,27 @@ class Genesis(object):
         self._set_timestamp()
         self["init_supply"] = 0
         self["init_rewards_supply"] = "1000000.000 SCORUM"
+
         self["accounts"] = []
+
         self["witness_candidates"] = []
+
+        self["registration_committee"] = []
+        self["registration_supply"] = "100.000 SCORUM"
+        self["registration_maximum_bonus"] = "0.100 SCORUM"
+        self["registration_schedule"] = [{"stage": 1,
+                                          "users": 1,
+                                          "bonus_percent": 100},
+                                         {"stage": 2,
+                                          "users": 2,
+                                          "bonus_percent": 75},
+                                         {"stage": 3,
+                                          "users": 5,
+                                          "bonus_percent": 50},
+                                         {"stage": 4,
+                                          "users": 10,
+                                          "bonus_percent": 25}
+                                         ]
 
     def __getitem__(self, item):
         return self.parms[item]
@@ -19,7 +38,9 @@ class Genesis(object):
     def __setitem__(self, key, value):
         self.parms[key] = value
 
-    def add_account(self, acc_name, public_key, recovery_account='', scr_amount=0, sp_amount=0,  witness=False):
+    def add_account(self, acc_name, public_key, recovery_account='', scr_amount=0, sp_amount=0,  witness=False, committee=False):
+        if len(self["registration_committee"]) < 1:
+            committee = True
         account = {"name": f"{acc_name}",
                    "recovery_account": f"{recovery_account}",
                    "public_key": f"{public_key}",
@@ -29,6 +50,8 @@ class Genesis(object):
         self['accounts'].append(account)
         if witness:
             self.add_witness(account["name"], account["public_key"])
+        if committee:
+            self["registration_committee"].append(account['name'])
 
     def add_witness(self, name, signing_key):
         witness = {"owner_name": f"{name}",
