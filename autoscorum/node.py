543995969c2716e96154d49d8995c3c612022c68
Maxim Kashevsky <maxim.kashevsky@scorum.com>
2018-05-18 19:14:23 +0300

    updated Node class set config as optional parameter paths and chain_params set as class attributes refactored setup method

--- autoscorum/node.py
+++ autoscorum/node.py
@@ -1,61 +1,67 @@
 import tempfile
 import os
 
-from pathlib import Path
 from hashlib import sha256
-
 from .config import Config
-from . import utils
 
 
 TEST_TEMP_DIR = '/tmp/autoscorum'
 SCORUM_BIN = 'scorumd'
 
 
-chain_params = {"chain_id": None,
-                "prefix": "SCR",
-                "scorum_symbol": "SCR",
-                "sp_symbol": "SP",
-                "scorum_prec": 9,
-                "sp_prec": 9}
-
-
 class Node(object):
-    def __init__(self, config=Config(), genesis=None, logging=True):
+    def __init__(self, config=None, genesis=None, logging=True):
         self.config = config
         self.genesis = genesis
         self.logging = logging
         self.logs = ""
         self.rpc_endpoint = None
-        self._dir_name = None
+        self.chain_params = None
+        self.work_dir = None
+        self.genesis_path = None
+        self.config_path = None
+        self._setup()
+        self._generate_configs()
 
     def get_chain_id(self):
-        if not chain_params["chain_id"]:
+        if not self.chain_params["chain_id"]:
             for line in self.logs:
                 if "node chain ID:" in line:
-                    chain_params["chain_id"] = line.split(" ")[-1]
-        return chain_params["chain_id"]
+                    self.chain_params["chain_id"] = line.split(" ")[-1]
+        return self.chain_params["chain_id"]
 
     def read_logs(self):
-        log_file = os.path.join(self._dir_name, 'logs/current')
+        log_file = os.path.join(self.work_dir, 'logs/current')
         with open(log_file, 'r') as logs:
             for line in logs:
                 self.logs += line
 
-    def setup(self):
-        self._dir_name = tempfile.mkdtemp(self.config['witness'][1:-1], dir=TEST_TEMP_DIR)
+    def _setup(self):
+        self.chain_params = {
+            "chain_id": None, "prefix": "SCR", "scorum_symbol": "SCR",
+            "sp_symbol": "SP", "scorum_prec": 9, "sp_prec": 9
+        }
 
-        genesis_path = os.path.join(self._dir_name, 'genesis.json')
-        config_path = os.path.join(self._dir_name, 'config.ini')
+        if self.config is None:
+            self.config = Config()
+            self.config['witness'] = '"dummy"'
 
-        if not os.path.exists(os.path.dirname(genesis_path)):
-            os.makedirs(os.path.dirname(genesis_path))
+        self.work_dir = tempfile.mkdtemp(
+            prefix=self.config['witness'][1:-1], dir=TEST_TEMP_DIR
+        )
 
-        with open(genesis_path, 'w') as genesis:
-            g = self.genesis.dump()
-            genesis.write(g)
-            chain_params["chain_id"] = sha256(g.encode()).hexdigest()
-        with open(config_path, 'w') as config:
-            config.write(self.config.dump())
+        self.genesis_path = os.path.join(self.work_dir, 'genesis.json')
+        self.config_path = os.path.join(self.work_dir, 'config.ini')
 
-        return os.path.dirname(genesis_path)
+    def _generate_configs(self):
+        if not os.path.exists(self.work_dir):
+            os.makedirs(self.work_dir)
+
+        if self.genesis is not None:
+            with open(self.genesis_path, 'w') as gfd:  # genesis file descriptor
+                g = self.genesis.dump()
+                self.chain_params["chain_id"] = sha256(g.encode()).hexdigest()
+                gfd.write(g)
+
+        with open(self.config_path, 'w') as cfd:  # config file descriptor
+            cfd.write(self.config.dump())
