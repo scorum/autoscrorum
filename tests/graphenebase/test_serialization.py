ad262e131e6ee384b4c5c50fad81feca63cf96bd
Maxim Kashevsky <maxim.kashevsky@scorum.com>
2018-08-30 12:36:06 +0300

    improved serializations tests

--- tests/graphenebase/test_serialization.py
+++ tests/graphenebase/test_serialization.py
@@ -2,6 +2,7 @@ from binascii import hexlify
 
 from graphenebase.types import BudgetType
 from graphenebase.amount import Amount
+from graphenebase.signedtransactions import SignedTransaction
 from src.operations_fabric import devpool_withdraw_vesting, create_budget_operation
 
 
@@ -29,15 +30,14 @@ def test_serialize_create_budget_to_byte():
     op = create_budget_operation(
         "initdelegate", "{}", Amount("10.000000000 SCR"), "2018-08-03T10:12:43", "2018-08-03T10:13:13", "post"
     )
-    result_bin = b'00000000000000000c696e697464656c6567617465027b7d00e40b540200000009534352000000009b2a645bb92a645b'
-    assert hexlify(bytes(op)) == result_bin
+    signed_ops = SignedTransaction.cast_operations_to_array_of_opklass([op])
+    result_bin = b'1a00000000000000000c696e697464656c6567617465027b7d00e40b540200000009534352000000009b2a645bb92a645b'
+    assert hexlify(bytes(signed_ops.data[0])) == result_bin
 
 
-def test_serialize_devpool_withdraw_vesting_proposal_create():
+def test_serialize_devpool_withdraw_vesting_proposal_create_to_byte():
     op = devpool_withdraw_vesting("initdelegate", Amount("10.000000000 SP"), 86400)
-    result_bin = b'0c696e697464656c6567617465805101000600e40b54020000000953500000000000'
-    # In C++   b'1d0c696e697464656c6567617465805101000600e40b54020000000953500000000000'
-    # The difference is only at the beginning - in result_bin is missing '1d'.
-    # That is hexlified id for ProposalCreate - hexlify(operationids.operations["proposal_create"])  == '1d'
-    # those bytes are added in SignedTransaction.cast_operations_to_array_of_opklass
-    assert hexlify(bytes(op)) == result_bin
+    signed_ops = SignedTransaction.cast_operations_to_array_of_opklass([op])
+
+    result_bin = b'1d0c696e697464656c6567617465805101000600e40b54020000000953500000000000'
+    assert hexlify(bytes(signed_ops.data[0])) == result_bin
