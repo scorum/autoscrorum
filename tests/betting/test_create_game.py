5c6c3a42140d2b3cdbb8ef0f615333281ad32a05
Maxim Kashevsky <maxim.kashevsky@scorum.com>
2018-10-24 18:33:30 +0300

    added test to check post_bet_operation; updated test for create_game (virt_ops check)

--- tests/betting/test_create_game.py
+++ tests/betting/test_create_game.py
@@ -2,11 +2,12 @@ from automation.wallet import Wallet
 from tests.betting.conftest import empower_betting_moderator
 from scorum.utils.time import fmt_time_from_now
 from scorum.graphenebase.betting import game, market
-from tests.common import validate_response, gen_uid
+from tests.common import validate_response, gen_uid, check_virt_ops
 
 
 def test_create_game(wallet: Wallet):
     empower_betting_moderator(wallet, 'alice')
-    validate_response(wallet.create_game(
-        gen_uid(), "alice", "game_name", fmt_time_from_now(10), 33, game.Soccer(), [market.Total(1000)]
-    ), wallet.create_game.__name__ )
+    response = wallet.create_game(
+        gen_uid(), "alice", "game_name", fmt_time_from_now(10), 33, game.Soccer(), [market.Total(1000)])
+    validate_response(response, wallet.create_game.__name__)
+    check_virt_ops(wallet, response['block_num'], response['block_num'], ["create_game"])
---
+++ tests/betting/test_post_bet.py
@@ -0,0 +1,23 @@
+import pytest
+
+from automation.wallet import Wallet
+from tests.betting.conftest import empower_betting_moderator, create_game
+from scorum.graphenebase.betting import wincase
+from scorum.graphenebase.amount import Amount
+from tests.common import validate_response, gen_uid, DEFAULT_WITNESS, check_virt_ops
+
+
+@pytest.mark.parametrize('better, wincase_type, odds, stake', [
+    ('alice', wincase.RoundHomeNo(), [2, 1], "5.000000000 SCR"),
+    ('bob', wincase.HandicapOver(-500), [3, 2], "3.000000000 SCR")
+])
+def test_post_bet(wallet: Wallet, better, wincase_type, odds, stake):
+    empower_betting_moderator(wallet, DEFAULT_WITNESS)
+    balance_before = wallet.get_account_scr_balance(better)
+    game_uuid = create_game(wallet, DEFAULT_WITNESS)
+    bet_uuid = gen_uid()
+    response = wallet.post_bet(bet_uuid, better, game_uuid, wincase_type, odds, stake, True)
+    validate_response(response, wallet.post_bet.__name__)
+    check_virt_ops(wallet, response['block_num'], response['block_num'], ["post_bet"])
+    balance_after = wallet.get_account_scr_balance(better)
+    assert balance_before - Amount(stake) == balance_after
