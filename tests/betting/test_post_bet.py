414557d82120f150f9cf192493178809237347e9
Maxim Kashevsky <maxim.kashevsky@scorum.com>
2018-11-05 18:29:38 +0300

    added test to check bets matching and stake calculations

--- tests/betting/test_post_bet.py
+++ tests/betting/test_post_bet.py
@@ -77,7 +77,8 @@ def test_post_bet_same_uuid(wallet_4hf: Wallet):
     game_uuid, _ = create_game(wallet_4hf, start=1, market_types=[market.RoundHome()])
     bet_uuid, _ = post_bet(wallet_4hf, "alice", game_uuid, wincase=wincase.RoundHomeYes())
     wallet_4hf.cancel_pending_bets([bet_uuid], "alice")
-    response = wallet_4hf.post_bet(bet_uuid, "alice", game_uuid, wincase.RoundHomeYes(), [3, 2], "1.000000000 SCR", True)
+    response = wallet_4hf.post_bet(
+        bet_uuid, "alice", game_uuid, wincase.RoundHomeYes(), [3, 2], "1.000000000 SCR", True)
     validate_error_response(response, wallet_4hf.post_bet.__name__, RE_OBJECT_EXIST)
 
 
@@ -103,12 +104,36 @@ def test_post_bet_auto_resolve(wallet_4hf: Wallet, bets):
 
 def test_post_bet_finished_game_resolve(wallet_4hf: Wallet, bets):
     change_resolve_delay(wallet_4hf, 3)  # resolve game next block after it will be finished
-    names = [b.account for b in bets]
-    accounts_before = {a["name"]: a for a in wallet_4hf.get_accounts(names)}
+    # names = [b.account for b in bets]
+    # accounts_before = {a["name"]: a for a in wallet_4hf.get_accounts(names)}
     game_uuid, bets_uuid = create_game_with_bets(wallet_4hf, bets, 1)
-    response = wallet_4hf.post_game_results(game_uuid, DEFAULT_WITNESS, [wincase.RoundHomeYes(), wincase.HandicapOver(500)])
+    response = wallet_4hf.post_game_results(
+        game_uuid, DEFAULT_WITNESS, [wincase.RoundHomeYes(), wincase.HandicapOver(500)])
     wallet_4hf.get_block(response["block_num"], wait_for_block=True)
-    accounts_after = {a["name"]: a for a in wallet_4hf.get_accounts(names)}
+    # accounts_after = {a["name"]: a for a in wallet_4hf.get_accounts(names)}
     assert wallet_4hf.get_pending_bets(bets_uuid) == []
     assert wallet_4hf.get_matched_bets(bets_uuid) == []
     # check rewards distribution
+
+
+def test_bets_matching(wallet_4hf: Wallet):
+    empower_betting_moderator(wallet_4hf)
+    game_uuid, _ = create_game(wallet_4hf, market_types=[market.RoundHome()])
+    alice_bet, _ = post_bet(
+        wallet_4hf, "alice", game_uuid, wincase_type=wincase.RoundHomeYes(),
+        odds=[3, 2], stake="1.000000000 SCR"  # potential reward 1,5 SCR, profit 0,5 SCR
+    )
+    bob_bet, block = post_bet(
+        wallet_4hf, "bob", game_uuid, wincase_type=wincase.RoundHomeNo(),
+        odds=[3, 1], stake="1.000000000 SCR"  # potential reward 3,0 SCR, profit 2,0 SCR
+    )  # total reward = alice bet 1.0 SCR + bob bet 0.5 SCR
+    check_virt_ops(wallet_4hf, block, block, ["bets_matched", "bet_cancelled", "bet_updated"])
+    pending_bets = wallet_4hf.lookup_pending_bets(-1, 100)
+    assert len(pending_bets) == 1, "Fully matched bet should be removed from pending bets"
+    assert pending_bets[0]["data"]["stake"] == "0.500000000 SCR", \
+        "Partially matched bet should remain with rest of initial stake/"
+    matched_bets = wallet_4hf.get_matched_bets([alice_bet, bob_bet])
+    assert matched_bets[0]["bet1_data"]["stake"] == "1.000000000 SCR", \
+        "Better with lesser potential reward should bet whole stake"
+    assert matched_bets[0]["bet2_data"]["stake"] == "0.500000000 SCR", \
+        "Better with greater potential reward should bet stake == profit of opponent"
